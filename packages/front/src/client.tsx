d556b9435b20aeed952bf321be28fe711b1271df
Leonid Fedorov <lfedorov@live.com>
2021-08-07 00:44:03 +0300

    chore: update dependencies (#4)

--- packages/front/src/client.tsx
+++ packages/front/src/client.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {hydrate} from 'react-dom';
+import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 
 import {App} from './app';
 
-hydrate(
+import './index.css';
+
+ReactDOM.hydrate(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
