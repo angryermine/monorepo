ca8602ad55a2a2a62bf2bbdc3c69c7c4f4a883e2
Leonid Fedorov <lfedorov@live.com>
2021-08-07 00:40:17 +0300

    chore: update dependencies

--- packages/admin/src/client.tsx
+++ packages/admin/src/client.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {hydrate} from 'react-dom';
+import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 
 import {App} from './app';
 
 import './index.css';
 
-hydrate(
+ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
