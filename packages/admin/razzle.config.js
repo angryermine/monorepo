321503ac877f1fcd9692f1ec6a2fd56556ee6630
Leonid Fedorov <lfedorov@live.com>
2021-10-27 01:29:14 +0300

    chore: remove tsconfig-paths-webpack-plugin (#15)

--- packages/admin/razzle.config.js
+++ packages/admin/razzle.config.js
@@ -1,12 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = {
-  modifyWebpackConfig({webpackConfig}) {
-    webpackConfig.resolve.plugins.push(new TsconfigPathsPlugin({}));
-
-    return webpackConfig;
-  },
   options: {
     buildType: 'spa',
   },
