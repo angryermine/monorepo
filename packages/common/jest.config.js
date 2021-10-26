2a6ddd6064fdaf816ca15e10f1083d9ba994b0ca
Leonid Fedorov <lfedorov@live.com>
2021-10-27 01:28:08 +0300

    chore: remove tsconfig-paths-webpack-plugin

--- packages/common/jest.config.js
+++ packages/common/jest.config.js
@@ -1,11 +1,6 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const {pathsToModuleNameMapper} = require('ts-jest/utils');
-const {compilerOptions} = require('./tsconfig.json');
-
 module.exports = {
   rootDir: 'src',
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {prefix: '<rootDir>'}),
   coverageDirectory: '<rootDir>/../coverage',
 };
